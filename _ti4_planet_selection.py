1bf5989c3f5b4c4c8bcd0774908ca7abf3262d5c
Dave Handley <dave@dah.me.uk>
2019-03-15 18:33:02 -0500

    Refactor formatters into config

--- _ti4_planet_selection.py
+++ _ti4_planet_selection.py
@@ -140,6 +140,58 @@ allocations = {
     },
 }
 
+# Configurations of various formatters
+formatters = {
+    "Text": {
+        "Title Pre": "",
+        "Title Post": "\n",
+        "Table Pre": "",
+        "System Pre": "  ",
+        "Col1 Pre": "Name: ",
+        "Col2 Pre": "; Resource: ",
+        "Col3 Pre": "; Influence: ",
+        "Col4 Pre": "; ",
+        "Col5 Pre": "",
+        "Col6 Pre": "",
+        "System Post": "\n",
+        "Table Post": "",
+        "Summary Pre": "  ",
+        "Summary Post": "\n",
+        "Planet Formatter": "{:22}",
+        "Error Pre": "",
+        "Error Post": "\n",
+    },
+    "HTML": {
+        "Title Pre": "<h2>",
+        "Title Post": "</h2>",
+        "Table Pre": (
+            "<table><tr>" +
+            "<th>System Name</th>" +
+            "<th>Resources</th>" +
+            "<th>Influence</th>" +
+            "<th>Wormhole</th>" +
+            "<th>Anomaly</th>" +
+            "<th>Blank</th>" +
+            "</tr>"
+        ),
+        "System Pre": "<tr><td>",
+        "Col1 Pre": "",
+        "Col2 Pre": "</td><td>",
+        "Col3 Pre": "</td><td>",
+        "Col4 Pre": "</td><td>",
+        "Col5 Pre": "</td><td>",
+        "Col6 Pre": "</td><td>",
+        "System Sep": "",
+        "System Post": "</td></tr>",
+        "Table Post": "</table>",
+        "Summary Pre": "<p><i>",
+        "Summary Post": "</i></p>",
+        "Planet Formatter": "{}",
+        "Error Pre": "<h2>Error</h2><p>",
+        "Error Post": "</p>",
+    },
+}
+
 # Extract reference data into working vectors
 # All tile names
 names = [tile[0] for tile in tiles]
@@ -402,35 +454,9 @@ def ti4_planet_selection(num_players, style, formatter=None):
     config = (int(num_players), str(style))
     # By default use an html formatter
     if formatter is None:
-        formatter = {
-            "Title Pre": "<h2>",
-            "Title Post": "</h2>",
-            "Table Pre": (
-                "<table><tr>" +
-                "<th>System Name</th>" +
-                "<th>Resources</th>" +
-                "<th>Influence</th>" +
-                "<th>Wormhole</th>" +
-                "<th>Anomaly</th>" +
-                "<th>Blank</th>" +
-                "</tr>"
-            ),
-            "System Pre": "<tr><td>",
-            "Col1 Pre": "",
-            "Col2 Pre": "</td><td>",
-            "Col3 Pre": "</td><td>",
-            "Col4 Pre": "</td><td>",
-            "Col5 Pre": "</td><td>",
-            "Col6 Pre": "</td><td>",
-            "System Sep": "",
-            "System Post": "</td></tr>",
-            "Table Post": "</table>",
-            "Summary Pre": "<p><i>",
-            "Summary Post": "</i></p>",
-            "Planet Formatter": "{}",
-            "Error Pre": "<h2>Error</h2><p>",
-            "Error Post": "</p>",
-        }
+        formatter = formatters["HTML"]
+    else:
+        formatter = formatters[formatter]
     results = None
     success = False
     if config not in allocations:
--- ti4_planet_selection.py
+++ ti4_planet_selection.py
@@ -13,25 +13,7 @@ def main():
     parser.add_argument(
         "-s", "--style", choices=style_options, default="default")
     args = parser.parse_args()
-    formatter = {
-        "Title Pre": "",
-        "Title Post": "\n",
-        "Table Pre": "",
-        "System Pre": "  ",
-        "Col1 Pre": "Name: ",
-        "Col2 Pre": "; Resource: ",
-        "Col3 Pre": "; Influence: ",
-        "Col4 Pre": "; ",
-        "Col5 Pre": "",
-        "Col6 Pre": "",
-        "System Post": "\n",
-        "Table Post": "",
-        "Summary Pre": "  ",
-        "Summary Post": "\n",
-        "Planet Formatter": "{:22}",
-        "Error Pre": "",
-        "Error Post": "\n",
-    }
+    formatter = "Text"
     option = (args.num_players, args.style)
     if option in options:
         print(ti4_planet_selection(args.num_players, args.style, formatter))
