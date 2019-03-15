03896122abbd31fc475317101d11d7ce2b982d53
Dave Handley <dave@dah.me.uk>
2019-03-15 16:39:16 -0500

    Fix formatting issues

--- ti4_planet_selection.py
+++ ti4_planet_selection.py
@@ -2,6 +2,7 @@
 import argparse
 from _ti4_planet_selection import ti4_planet_selection, config_options
 
+
 # Main function
 def main():
     parser = argparse.ArgumentParser(description="Allocate tiles for TI4")
@@ -9,7 +10,8 @@ def main():
     num_players_options = sorted(list(set([option[0] for option in options])))
     style_options = sorted(list(set([option[1] for option in options])))
     parser.add_argument("num_players", type=int, choices=num_players_options)
-    parser.add_argument("-s", "--style", choices=style_options, default="default")
+    parser.add_argument(
+        "-s", "--style", choices=style_options, default="default")
     args = parser.parse_args()
     formatter = {
         "Title Pre": "",
@@ -32,7 +34,9 @@ def main():
     if option in options:
         print(ti4_planet_selection(args.num_players, args.style, formatter))
     else:
-        raise RuntimeError("Invalid configuration, valid choices are:\n{}".format(options))
+        raise RuntimeError(
+            "Invalid configuration, valid choices are:\n{}".format(options))
+
 
 if __name__ == "__main__":
     main()
